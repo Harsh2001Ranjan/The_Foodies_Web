33a8c424bafaf7a6828b481f4566b15327036726
Harsh <Harsh@example.com>
2024-06-13 00:13:31 +0530

    Created UI of Contact page And Cart Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import "./styles/app.scss";
+import "./styles/headers.scss";
+import "./styles/home.scss";
 import "./styles/Founder.scss";
 import "./styles/menu.scss";
 import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/cart.scss";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Contact from "./components/contact/Contact";
+import Cart from "./components/cart/Cart";
 function App() {
   return (
     <Router>
@@ -13,8 +20,9 @@ function App() {
       {/* isAuthenticated={true} */}
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        {/* <Route path="/about" element={<About />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/shipping" element={<Shipping />} />
         <Route path="/confirmorder" element={<ConfirmOrder />} />
