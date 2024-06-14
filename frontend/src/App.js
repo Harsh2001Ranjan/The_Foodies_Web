35970089b3e3239dea420635e8d545d5943f7cb0
Vaishnavi Srivastava <vaishnavisrivastava864@gmail.com>
2024-06-14 16:57:52 +0530

    created order confirmation, payment status,login,profile and orderdetails' page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,12 @@ import "./styles/footer.scss";
 import "./styles/contact.scss";
 import "./styles/cart.scss";
 import "./styles/shipping.scss";
+import "./styles/confirmOrder.scss";
+import "./styles/paymentsuccess.scss";
+import "./styles/login.scss";
+import "./styles/profile.scss";
+import "./styles/table.scss";
+import "./styles/orderDetails.scss";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
@@ -15,31 +21,37 @@ import Footer from "./components/layout/Footer";
 import Contact from "./components/contact/Contact";
 import Cart from "./components/cart/Cart";
 import Shipping from "./components/cart/Shipping";
+import ConfirmOrder from "./components/cart/ConfirmOrder";
+import PaymentSuccess from "./components/cart/PaymentSuccess";
+import Login from "./components/login/Login";
+import Profile from "./components/profile/Profile";
+import MyOrders from "./components/myOrders/MyOrders";
+import OrderDetails from "./components/myOrders/OrderDetails";
+
 function App() {
   return (
     <Router>
-      <Header />
-      {/* isAuthenticated={true} */}
+      <Header isAuthenticated={true} />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/shipping" element={<Shipping />} />
-        {/* <Route path="/about" element={<About />} />
-        
         <Route path="/confirmorder" element={<ConfirmOrder />} />
-        <Route path="/paymentsuccess" element={<PaymentSuccess />} />
+        <Route path="/paymentsuccess" element={<PaymentSuccess />} />{" "}
         <Route path="/login" element={<Login />} />
         <Route path="/me" element={<Profile />} />
         <Route path="/myorders" element={<MyOrders />} />
         <Route path="/order/:id" element={<OrderDetails />} />
+        {/* <Route path="/about" element={<About />} /> */}
+        {/*
         <Route path="/admin/dashboard" element={<Dashboard />} />
         <Route path="/admin/users" element={<Users />} />
         <Route path="/admin/orders" element={<Orders />} />
 
         <Route path="*" element={<NotFound />} /> */}
       </Routes>
-
       <Footer />
     </Router>
   );
